3cea63344ae434907989be92667598cc398d13fe
sergiosanturde <72124215+sergiosanturde@users.noreply.github.com>
2023-04-15 11:23:29 +0200

    update
    Añadido clases Perfil/Usuario

---
+++ PruebasProyecto/app/src/main/java/com/example/pruebasproyecto/model/Perfil.kt
@@ -0,0 +1,4 @@
+package com.example.pruebasproyecto.model
+
+class Perfil(var idMini:Int,var nombre:String,var especie:String,var universo:String,var nMinis:Int,var imagen:String) {
+}
---
+++ PruebasProyecto/app/src/main/java/com/example/pruebasproyecto/model/Usuario.kt
@@ -0,0 +1,4 @@
+package com.example.pruebasproyecto.model
+
+class Usuario(var id: Int,var correo:String,var fav:ArrayList<ArrayList<Any>>) {
+}

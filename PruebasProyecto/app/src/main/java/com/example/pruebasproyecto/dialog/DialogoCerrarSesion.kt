af0959b89478edade4c51c2dd6d23c805b0921b2
sergiosanturde <72124215+sergiosanturde@users.noreply.github.com>
2023-05-24 13:19:36 +0200

    Update DialogoCerrarSesion

--- PruebasProyecto/app/src/main/java/com/example/pruebasproyecto/dialog/DialogoCerrarSesion.kt
+++ PruebasProyecto/app/src/main/java/com/example/pruebasproyecto/dialog/DialogoCerrarSesion.kt
@@ -3,6 +3,7 @@ package com.example.pruebasproyecto.dialog
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.DialogFragment
 
 class DialogoCerrarSesion: DialogFragment() {
@@ -10,5 +11,18 @@ class DialogoCerrarSesion: DialogFragment() {
 
         var builder = AlertDialog.Builder(requireContext());
         builder.setTitle("Confirmación de salida")
+            .setMessage("¿Seguro que quieres cerrar la sesión?")
+            .setPositiveButton("Aceptar") { dialogInterface, posicion ->
+                Log.v(
+                    "dialogos",
+                    "Pulsado aceptar "+posicion.toString()
+                )
+            }.setNegativeButton("Cancelar"){ dialogInterface, posicion ->
+                Log.v(
+                    "dialogos",
+                    "Pulsado cancelar "+posicion.toString()
+                )
+            }
+        return builder.create()
     }
 }
